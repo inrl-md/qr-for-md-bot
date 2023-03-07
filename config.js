884d7bb39c8f28b519a2ff046a937b922ed98702
inrl-md <109638623+inrl-md@users.noreply.github.com>
2023-03-07 11:26:55 +0530

    addod

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 module.exports = {
        MONGO_URL : '',//enter your mongodb url to get and store youe session!
        SESSION_PREFIX : 'inrl~'// defult value of the satrting of your session
+       GROUP_URL :"https://chat.whatsapp.com/group?inrl=groupid",//group url for your aupport grp link
+       GITHUB_URL : "https://github.com/ekash.efx/inrl/fork"//github id for fork after scaning
 }
 /*
       EXAMPLE
